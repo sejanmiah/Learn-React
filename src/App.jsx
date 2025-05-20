5770cc7db462c72e63b83f9d0b312be85ee52f57
sejanmiah <sejankhan931@gmail.com>
2025-05-21 00:15:15 +0400

    Put the array on useState-artist

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import './App.css'
 
+let initialArtists = [
+  { id: 0, name: 'Marta Colvin Andrade' },
+  { id: 1, name: 'Lamidi Olonade Fakeye'},
+  { id: 2, name: 'Louise Nevelson'},
+];
+// console.log(initialArtists);
+
+
+
 function App() {
+  const [artists, setArtists] = useState(initialArtists);
+  // console.log(artists);
 
 
   return (
